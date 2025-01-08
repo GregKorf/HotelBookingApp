8899a532f0f4d333111fdbe3fc6cd0a2fc58ce72
Gregory Korfiatis <gregkorf10@gmail.com>
2025-01-08 13:34:13 +0200

    Add Repositories

---
+++ HotelBookingApp/Repositories/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿
+using HotelBookingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingApp.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly HotelBookingDbContext context;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(HotelBookingDbContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+
+        public virtual async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+
+        public virtual Task UpdateAsync(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            T? existingEntity = await GetByIdAsync(id);
+
+            if(existingEntity is null) return false;
+
+            dbSet.Remove(existingEntity);
+
+            return true;
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id) => await dbSet.FindAsync(id);
+
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync() => await dbSet.ToListAsync();
+
+    }
+}
